155705af7c304ba2f317558aa46f660f8f666879
Jose Estrada <webdeveloper@vgloreto.com>
2020-02-04 16:55:50 -0600

    politicas de privacidad

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get '/quien-soy', to: 'pages#quienes'
+  get '/politicas-de-privacidad', to: 'pages#politicas'
   #get 'portafolio', to: 'pages#portafolio'
   #get 'blog', to: 'pages#blog'
 end
